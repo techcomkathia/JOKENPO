5159f751bece4c6bc96fbb7912579cc4f3b31b7a
Kelvys Moura <kelvysmoura2@gmail.com>
2024-07-08 17:09:18 -0300

    primeira parte

---
+++ script.js
@@ -0,0 +1,17 @@
+const opcoes = {
+    1 : { nome: 'pedra', img:'img/pedra.png', pedra: 'EMPATE', papel : 'PERDEU', tesoura: 'GANHOU'},
+    2 : { nome: 'papel', img:'img/papel.png', pedra: 'GANHOU', papel : 'EMPATE', tesoura: 'PERDEU'},
+    3 : { nome: 'tesoura', img:'img/tesoura.png', pedra: 'PERDEU', papel : 'GANHOU', tesoura: 'EMPATE'}
+}
+
+let escolhaJogador= {}
+let escolhaComputador= {}
+
+function escolherJogada(num){
+    escolhaJogador= opcoes[num]
+}
+
+let numComp = Math.floor((Math.random()*3)+1)
+escolhaComputador = opcoes[numComp]
+
+
