207f9900967eecdda5894f2fa018d5e523a6a20f
Kelvys Moura <kelvysmoura2@gmail.com>
2024-07-09 14:13:52 -0300

    funções de manipulação da tela

--- script.js
+++ script.js
@@ -4,14 +4,85 @@ const opcoes = {
     3 : { nome: 'tesoura', img:'img/tesoura.png', pedra: 'PERDEU', papel : 'GANHOU', tesoura: 'EMPATE'}
 }
 
+//seleção dos objetos do document
+//imagem e texto da escolha do usuário
+let imgJogador = document.getElementById('imgUser')
+let pJogador =  document.getElementById('escolhaUsuario')
+
+//imagem e texto da escolha do computador
+let imgComputador = document.getElementById('imgComput')
+let pComputador =  document.getElementById('escolhaComputador')
+
+//resultado final
+let resultadoFinal = document.getElementById('resultado')
+
+//contagem dos pontos no document
+let contagemPontosComputador = document.getElementById('pontosComputador')
+let contagemPontosJogador = document.getElementById('pontosJogador')
+
+//variáveis globais para escolha do usuário e do computador
 let escolhaJogador= {}
 let escolhaComputador= {}
 
+//variáveis globais para pontuação dos jogadores
+let pontosJogador = 0
+let pontosComputador = 0
+
 function escolherJogada(num){
-    escolhaJogador= opcoes[num]
+    return opcoes[num]
 }
 
-let numComp = Math.floor((Math.random()*3)+1)
-escolhaComputador = opcoes[numComp]
+function montarJogadas(numJogador){
+    let numComp = Math.floor((Math.random()*3)+1)
+    escolhaJogador = escolherJogada(numJogador)
+    escolhaComputador = escolherJogada(numComp)
+}
+
+function avaliarGanhador(){
+
+    if( escolhaJogador[escolhaComputador.nome] == 'GANHOU'){
+        pontosJogador += 1
+    }
+    else if (escolhaJogador[escolhaComputador.nome] == 'PERDEU'){
+        pontosComputador += 1 
+    }
+}
+
+function montarTela(){
+    imgJogador.src= escolhaJogador.img
+    imgComputador.src = escolhaComputador.img
+
+    imgJogador.alt= escolhaJogador.nome
+    imgComputador.alt = escolhaComputador.nome
+
+    pJogador.innerHTML = ` Você escolheu ${escolhaJogador.nome}`
+    pComputador.innerHTML = `O Computador escolheu ${escolhaComputador.nome}`
+
+    contagemPontosComputador = pontosComputador
+    contagemPontosJogador = pontosJogador
+
+    resultadoFinal.innerHTML= escolhaJogador[escolhaComputador.nome]
+
+    //adicção da classe para manipulação do style
+    switch(escolhaJogador[escolhaComputador.nome]){
+        case 'GANHOU':
+            resultadoFinal.classList = 'ganhou'
+            break
+        case 'PERDEU':
+            resultadoFinal.classList = 'perdeu'
+            break
+        default:
+            resultadoFinal.classList = 'empatou'
+            break
+    }
+
+}
+
+function jogar(num){
+    montarJogadas(num)
+    avaliarGanhador()
+    montarTela()
+}
 
 
+jogar(1)
